3cc0aa659bb723c1fe9085dcc57dd259b6ce43f8
Ben Diesel <befkotze@gmail.com>
2025-09-23 07:40:38 +0200

    Implement events query

---
+++ prisma/queries/get-events-with-user-teams.ts
@@ -0,0 +1,21 @@
+import type { PrismaClient } from "@/generated/prisma";
+
+export const getEventsWithUserTeams =
+	(prismaClient: PrismaClient) => (user_id: string) =>
+		prismaClient.events.findMany({
+			include: {
+				teams: {
+					where: {
+						user_teams: {
+							some: {
+								user_id,
+							},
+						},
+					},
+				},
+			},
+		});
+
+export type EventWithUserTeam = Awaited<
+	ReturnType<ReturnType<typeof getEventsWithUserTeams>>
+>[0];
